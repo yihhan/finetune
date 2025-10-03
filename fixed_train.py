4eb7322cc7e7227c06b4795189cbe80d0e6dcd5a
Yih Han Tan <yih.han.tan@ibm.com>
2025-10-03 16:08:03 +0800

    Fix TrainingArguments parameter: evaluation_strategy -> eval_strategy

--- fixed_train.py
+++ fixed_train.py
@@ -250,7 +250,7 @@ def create_fixed_training_arguments(
         eval_steps=eval_steps,
         save_steps=save_steps,
         save_total_limit=2,
-        evaluation_strategy="steps",
+        eval_strategy="steps",
         load_best_model_at_end=True,
         metric_for_best_model="eval_loss",
         greater_is_better=False,
